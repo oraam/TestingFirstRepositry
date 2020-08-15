592da222d66215904bb0cdc06fe6ba71689f89ac
Rohit Mudgal <humanity.generator@gmail.com>
2020-08-15 21:13:13 +0530

    Create WelcomeIndia.py

---
+++ WelcomeIndia.py
@@ -0,0 +1,24 @@
+  Print("Hello India")
+  Print("Bryan O'Sullivan
+Bryan (@bos31337) is the author of Real World Haskell and co-author of Mercurial: The Definitive Guide, both published by O'Reilly. He also co-authored The Jini Specification. He is Engineering Director at Facebook where he manages the Developer Efficiency team, and he lectures at Stanford University.
+
+Jeff Atwood
+Jeff (@codinghorror) is Co-founder of stackoverflow.com and stackexchange.com. While his profile includes the disclaimer that he has no idea what he's talking about, 82,000 followers might disagree. His blog post on The Future of Markdown urges better standardization and lists needed tweaks for the Markdown language.
+
+Vanessa Hurst
+Vanessa (@DBNess) describes herself as a coder and lifetime Girl Scout! She aims to empower coders everywhere through CodeMontage and she co-founded Girl Develop It, which aims to provide web and software development training to women from diverse backgrounds.
+
+Mina Markham
+Mina (@MinaMarkham) is a self-confessed STEMinist and front-end developer. She was a front-end engineer for Hillary Clinton’s 2016 presidential campaign. Mina’s big into public speaking and presents at conferences like Sass Summit, Front-End Design Conference, and Midwest.io.
+
+K. Scott Allen
+Allen (@OdeToCode) has over 25 years’ commercial software development experience in C#, ASP.NET, ASP.NET MVC and SQL. He authored What Every JavaScript Developer Should Know About ECMAScript 2015 and What Every Web Developer Should Know About HTTP.
+
+Alex Payne
+Alex (@al3x) is a programmer, writer, and self-described secular humanist. He is the co-author of Programming Scala published by O'Reilly and is an expert on emerging programming languages and technologies. Payne was formerly the CTO of Simple and before that helped build Twitter's developer platform as one of their first employees in 2007.
+
+Amber Conville
+Amber (@crebma) calls herself a codeasaurus rex and is a developer at Test Double. Amber is the organizer of Self.conference, a Detroit-based gathering full of hot tech presentations and insightful talks.
+
+Jason Fried
+Jason (@jasonfried) co-authored the New York Times bestseller Rework with David Heinemeier Hansson. Together they founded 37Signals.com, which built simple but powerful collaboration tools like Basecamp, Highrise, Ta-da List, and Writeboard. "[Facebook and Twitter] aren't the real problems in the office," Fried says in a perceptive TED talk, "The real problems are what I like to call the M&Ms, the Managers and the Meetings")
