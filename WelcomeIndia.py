63801dfdbc0a3ea46cedabd707b873a659edbffa
Rohit Mudgal <humanity.generator@gmail.com>
2020-08-15 21:18:55 +0530

    Indian States & Districts

--- WelcomeIndia.py
+++ WelcomeIndia.py
@@ -1,24 +1,709 @@
   Print("Hello India")
-  Print("Bryan O'Sullivan
-Bryan (@bos31337) is the author of Real World Haskell and co-author of Mercurial: The Definitive Guide, both published by O'Reilly. He also co-authored The Jini Specification. He is Engineering Director at Facebook where he manages the Developer Efficiency team, and he lectures at Stanford University.
+  Print("1	Andaman & Nicobar Islands	Page
+(1-1)	Nicobars 	
+(1-2)	North & Middle Andaman	
+(1-3)	South Andaman	
 
-Jeff Atwood
-Jeff (@codinghorror) is Co-founder of stackoverflow.com and stackexchange.com. While his profile includes the disclaimer that he has no idea what he's talking about, 82,000 followers might disagree. His blog post on The Future of Markdown urges better standardization and lists needed tweaks for the Markdown language.
+2	Andhra Pradesh	
+(2-1)	Anantapur 	
+(2-2)	Chittoor 	
+(2-3)	East Godavari 	
+(2-4)	Guntur 	
+(2-5)	Krishna 	
+(2-6)	Kurnool 	
+(2-7)	Prakasam 	
+(2-8)	Sri Potti Sriramulu Nellore 	
+(2-9)	Srikakulam 	
+(2-10)	Visakhapatnam 	
+(2-11)	Vizianagaram 	
+(2-12)	West Godavari 	
+(2-13)	Y S R	
 
-Vanessa Hurst
-Vanessa (@DBNess) describes herself as a coder and lifetime Girl Scout! She aims to empower coders everywhere through CodeMontage and she co-founded Girl Develop It, which aims to provide web and software development training to women from diverse backgrounds.
+3	Arunachal Pradesh	
+(3-1)	Anjaw 	
+(3-2)	Changlang 	
+(3-3)	Dibang Valley 	
+(3-4)	East Kameng 	
+(3-5)	East Siang 	
+(3-6)	Kurung Kumey 	
+(3-7)	Lohit 	
+(3-8)	Lower Dibang Valley 	
+(3-9)	Lower Subansiri 	
+(3-10)	Papum Pare 	
+(3-11)	Tawang 	
+(3-12)	Tirap 	
+(3-13)	Upper Siang 	
+(3-14)	Upper Subansiri 	
+(3-15)	West Kameng 	
+(3-16)	West Siang	
 
-Mina Markham
-Mina (@MinaMarkham) is a self-confessed STEMinist and front-end developer. She was a front-end engineer for Hillary Clinton’s 2016 presidential campaign. Mina’s big into public speaking and presents at conferences like Sass Summit, Front-End Design Conference, and Midwest.io.
+4	Assam	
+(4-1)	Baksa 	
+(4-2)	Barpeta 	
+(4-3)	Bongaigaon 	
+(4-4)	Cachar 	
+(4-5)	Chirang 	
+(4-6)	Darrang 	
+(4-7)	Dhemaji 	
+(4-8)	Dhubri 	
+(4-9)	Dibrugarh 	
+(4-10)	Dima Hasao 	
+(4-11)	Goalpara 	
+(4-12)	Golaghat 	
+(4-13)	Hailakandi 	
+(4-14)	Jorhat 	
+(4-15)	Kamrup 	
+(4-16)	Kamrup Metropolitan 	
+(4-17)	Karbi Anglong 	
+(4-18)	Karimganj 	
+(4-19)	Kokrajhar 	
+(4-20)	Lakhimpur 	
+(4-21)	Morigaon 	
+(4-22)	Nagaon 	
+(4-23)	Nalbari 	
+(4-24)	Sivasagar 	
+(4-25)	Sonitpur 	
+(4-26)	Tinsukia 	
+(4-27)	Udalguri	
 
-K. Scott Allen
-Allen (@OdeToCode) has over 25 years’ commercial software development experience in C#, ASP.NET, ASP.NET MVC and SQL. He authored What Every JavaScript Developer Should Know About ECMAScript 2015 and What Every Web Developer Should Know About HTTP.
+5	Bihar	
+(5-1)	Araria 	
+(5-2)	Arwal 	
+(5-3)	Aurangabad 	
+(5-4)	Banka 	
+(5-5)	Begusarai 	
+(5-6)	Bhagalpur 	
+(5-7)	Bhojpur 	
+(5-8)	Buxar 	
+(5-9)	Darbhanga 	
+(5-10)	Gaya 	
+(5-11)	Gopalganj 	
+(5-12)	Jamui 	
+(5-13)	Jehanabad 	
+(5-14)	Kaimur 	
+(5-15)	Katihar 	
+(5-16)	Khagaria 	
+(5-17)	Kishanganj 	
+(5-18)	Lakhisarai 	
+(5-19)	Madhepura 	
+(5-20)	Madhubani 	
+(5-21)	Munger 	
+(5-22)	Muzaffarpur 	
+(5-23)	Nalanda 	
+(5-24)	Nawada 	
+(5-25)	Pashchim Champaran 	
+(5-26)	Patna 	
+(5-27)	Purba Champaran 	
+(5-28)	Purnia 	
+(5-29)	Rohtas 	
+(5-30)	Saharsa 	
+(5-31)	Samastipur 	
+(5-32)	Saran 	
+(5-33)	Sheikhpura 	
+(5-34)	Sheohar 	
+(5-35)	Sitamarhi 	
+(5-36)	Siwan 	
+(5-37)	Supaul 	
+(5-38)	Vaishali	
 
-Alex Payne
-Alex (@al3x) is a programmer, writer, and self-described secular humanist. He is the co-author of Programming Scala published by O'Reilly and is an expert on emerging programming languages and technologies. Payne was formerly the CTO of Simple and before that helped build Twitter's developer platform as one of their first employees in 2007.
+6	Chandigarh	
+(6-1)	Chandigarh	
 
-Amber Conville
-Amber (@crebma) calls herself a codeasaurus rex and is a developer at Test Double. Amber is the organizer of Self.conference, a Detroit-based gathering full of hot tech presentations and insightful talks.
+7	Chhattisgarh	
+(7-1)	Bastar 	
+(7-2)	Bijapur 	
+(7-3)	Bilaspur 	
+(7-4)	Dakshin Bastar Dantewada 	
+(7-5)	Dhamtari 	
+(7-6)	Durg 	
+(7-7)	Janjgir Champa 	
+(7-8)	Jashpur 	
+(7-9)	Kabeerdham 	
+(7-10)	Korba 	
+(7-11)	Koriya 	
+(7-12)	Mahasamund 	
+(7-13)	Narayanpur 	
+(7-14)	Raigarh 	
+(7-15)	Raipur 	
+(7-16)	Rajnandgaon 	
+(7-17)	Surguja 	
+(7-18)	Uttar Bastar Kanker	
 
-Jason Fried
-Jason (@jasonfried) co-authored the New York Times bestseller Rework with David Heinemeier Hansson. Together they founded 37Signals.com, which built simple but powerful collaboration tools like Basecamp, Highrise, Ta-da List, and Writeboard. "[Facebook and Twitter] aren't the real problems in the office," Fried says in a perceptive TED talk, "The real problems are what I like to call the M&Ms, the Managers and the Meetings")
+8	Dadra & Nagar Haveli	
+(8-1)	Dadra & Nagar Haveli	
+
+9	Daman & Diu	
+(9-1)	Daman 	
+(9-2)	Diu	
+
+10	Delhi	
+(10-1)	Chandni Chowk	
+(10-2)	East Delhi	
+(10-3)	New Delhi	
+(10-4)	North East Delhi	
+(10-5)	North West Delhi	
+(10-6)	West Delhi	
+(10-7)	South Delhi	
+
+11	Goa	
+(11-1)	North Goa 	
+(11-2)	South Goa	
+
+12	Gujarat	
+(12-1)	Ahmadabad 	
+(12-2)	Amreli 	
+(12-3)	Anand 	
+(12-4)	Banas Kantha 	
+(12-5)	Bharuch 	
+(12-6)	Bhavnagar 	
+(12-7)	Dohad 	
+(12-8)	Gandhinagar 	
+(12-9)	Jamnagar 	
+(12-10)	Junagadh 	
+(12-11)	Kachchh 	
+(12-12)	Kheda 	
+(12-13)	Mahesana 	
+(12-14)	Narmada 	
+(12-15)	Navsari 	
+(12-16)	Panch Mahals 	
+(12-17)	Patan 	
+(12-18)	Porbandar 	
+(12-19)	Rajkot 	
+(12-20)	Sabar Kantha 	
+(12-21)	Surat 	
+(12-22)	Surendranagar 	
+(12-23)	Tapi 	
+(12-24)	The Dangs 	
+(12-25)	Vadodara 	
+(12-26)	Valsad	
+
+13	Haryana	
+(13-1)	Ambala 	
+(13-2)	Bhiwani 	
+(13-3)	Faridabad 	
+(13-4)	Fatehabad 	
+(13-5)	Gurgaon 	
+(13-6)	Hisar 	
+(13-7)	Jhajjar 	
+(13-8)	Jind 	
+(13-9)	Kaithal 	
+(13-10)	Karnal 	
+(13-11)	Kurukshetra 	
+(13-12)	Mahendragarh 	
+(13-13)	Mewat 	
+(13-14)	Palwal 	
+(13-15)	Panchkula 	
+(13-16)	Panipat 	
+(13-17)	Rewari 	
+(13-18)	Rohtak 	
+(13-19)	Sirsa 	
+(13-20)	Sonipat 	
+(13-21)	Yamunanagar	
+
+14	Himachal Pradesh	
+(14-1)	Bilaspur 	
+(14-2)	Chamba 	
+(14-3)	Hamirpur 	
+(14-4)	Kangra 	
+(14-5)	Kinnaur 	
+(14-6)	Kullu 	
+(14-7)	Lahul & Spiti 	
+(14-8)	Mandi 	
+(14-9)	Shimla 	
+(14-10)	Sirmaur 	
+(14-11)	Solan 	
+(14-12)	Una	
+
+15	Jammu & Kashmir	
+(15-1)	Anantnag 	
+(15-2)	Badgam 	
+(15-3)	Bandipore 	
+(15-4)	Baramula 	
+(15-5)	Doda 	
+(15-6)	Ganderbal 	
+(15-7)	Jammu 	
+(15-8)	Kargil 	
+(15-9)	Kathua 	
+(15-10)	Kishtwar 	
+(15-11)	Kulgam 	
+(15-12)	Kupwara 	
+(15-13)	Leh 	
+(15-14)	Pulwama 	
+(15-15)	Punch 	
+(15-16)	Rajouri 	
+(15-17)	Ramban 	
+(15-18)	Reasi 	
+(15-19)	Samba 	
+(15-20)	Shupiyan 	
+(15-21)	Srinagar 	
+(15-22)	Udhampur	
+
+16	Jharkhand	
+(16-1)	Bokaro 	
+(16-2)	Chatra 	
+(16-3)	Deoghar 	
+(16-4)	Dhanbad 	
+(16-5)	Dumka 	
+(16-6)	Garhwa 	
+(16-7)	Giridih 	
+(16-8)	Godda 	
+(16-9)	Gumla 	
+(16-10)	Hazaribagh 	
+(16-11)	Jamtara 	
+(16-12)	Khunti 	
+(16-13)	Kodarma 	
+(16-14)	Latehar 	
+(16-15)	Lohardaga 	
+(16-16)	Pakur 	
+(16-17)	Palamu 	
+(16-18)	Pashchimi Singhbhum 	
+(16-19)	Purbi Singhbhum 	
+(16-20)	Ramgarh 	
+(16-21)	Ranchi 	
+(16-22)	Sahibganj 	
+(16-23)	Saraikela Kharsawan 	
+(16-24)	Simdega	
+
+17	Karnataka	
+(17-1)	Bagalkot 	
+(17-2)	Bangalore 	
+(17-3)	Bangalore Rural 	
+(17-4)	Belgaum 	
+(17-5)	Bellary 	
+(17-6)	Bidar 	
+(17-7)	Bijapur 	
+(17-8)	Chamarajanagar 	
+(17-9)	Chikkaballapura 	
+(17-10)	Chikmagalur 	
+(17-11)	Chitradurga 	
+(17-12)	Dakshina Kannada 	
+(17-13)	Davanagere 	
+(17-14)	Dharwad 	
+(17-15)	Gadag 	
+(17-16)	Gulbarga 	
+(17-17)	Hassan 	
+(17-18)	Haveri 	
+(17-19)	Kodagu 	
+(17-20)	Kolar 	
+(17-21)	Koppal 	
+(17-22)	Mandya 	
+(17-23)	Mysore 	
+(17-24)	Raichur 	
+(17-25)	Ramanagara 	
+(17-26)	Shimoga 	
+(17-27)	Tumkur 	
+(17-28)	Udupi 	
+(17-29)	Uttara Kannada 	
+(17-30)	Yadgir	
+
+18	Kerala	
+(18-1)	Alappuzha 	
+(18-2)	Ernakulam 	
+(18-3)	Idukki 	
+(18-4)	Kannur 	
+(18-5)	Kasaragod 	
+(18-6)	Kollam 	
+(18-7)	Kottayam 	
+(18-8)	Kozhikode 	
+(18-9)	Malappuram 	
+(18-10)	Palakkad 	
+(18-11)	Pathanamthitta 	
+(18-12)	Thiruvananthapuram 	
+(18-13)	Thrissur 	
+(18-14)	Wayanad	
+
+19	Lakshadweep	
+(19-1)	Lakshadweep	
+
+20	Madhya Pradesh	
+(20-1)	Alirajpur 	
+(20-2)	Anuppur 	
+(20-3)	Ashoknagar 	
+(20-4)	Balaghat 	
+(20-5)	Barwani 	
+(20-6)	Betul 	
+(20-7)	Bhind 	
+(20-8)	Bhopal 	
+(20-9)	Burhanpur 	
+(20-10)	Chhatarpur 	
+(20-11)	Chhindwara 	
+(20-12)	Damoh 	
+(20-13)	Datia 	
+(20-14)	Dewas 	
+(20-15)	Dhar 	
+(20-16)	Dindori 	
+(20-17)	Guna 	
+(20-18)	Gwalior 	
+(20-19)	Harda 	
+(20-20)	Hoshangabad 	
+(20-21)	Indore 	
+(20-22)	Jabalpur 	
+(20-23)	Jhabua 	
+(20-24)	Katni 	
+(20-25)	Khandwa 	
+(20-26)	Khargone 	
+(20-27)	Mandla 	
+(20-28)	Mandsaur 	
+(20-29)	Morena 	
+(20-30)	Narsimhapur 	
+(20-31)	Neemuch 	
+(20-32)	Panna 	
+(20-33)	Raisen 	
+(20-34)	Rajgarh 	
+(20-35)	Ratlam 	
+(20-36)	Rewa 	
+(20-37)	Sagar 	
+(20-38)	Satna 	
+(20-39)	Sehore 	
+(20-40)	Seoni 	
+(20-41)	Shahdol 	
+(20-42)	Shajapur 	
+(20-43)	Sheopur 	
+(20-44)	Shivpuri 	
+(20-45)	Sidhi 	
+(20-46)	Singrauli 	
+(20-47)	Tikamgarh 	
+(20-48)	Ujjain 	
+(20-49)	Umaria 	
+(20-50)	Vidisha	
+
+21	Maharashtra	
+(21-1)	Ahmadnagar 	
+(21-2)	Akola 	
+(21-3)	Amravati 	
+(21-4)	Aurangabad 	
+(21-5)	Bhandara 	
+(21-6)	Bid 	
+(21-7)	Buldana 	
+(21-8)	Chandrapur 	
+(21-9)	Dhule 	
+(21-10)	Gadchiroli 	
+(21-11)	Gondiya 	
+(21-12)	Hingoli 	
+(21-13)	Jalgaon 	
+(21-14)	Jalna 	
+(21-15)	Kolhapur 	
+(21-16)	Latur 	
+(21-17)	Mumbai 	
+(21-18)	Mumbai Suburban 	
+(21-19)	Nagpur 	
+(21-20)	Nanded 	
+(21-21)	Nandurbar 	
+(21-22)	Nashik 	
+(21-23)	Osmanabad 	
+(21-24)	Parbhani 	
+(21-25)	Pune 	
+(21-26)	Raigarh 	
+(21-27)	Ratnagiri 	
+(21-28)	Sangli 	
+(21-29)	Satara 	
+(21-30)	Sindhudurg 	
+(21-31)	Solapur 	
+(21-32)	Thane 	
+(21-33)	Wardha 	
+(21-34)	Washim 	
+(21-35)	Yavatmal	
+
+22	Manipur	
+(22-1)	Bishnupur 	
+(22-2)	Chandel 	
+(22-3)	Churachandpur 	
+(22-4)	Imphal East 	
+(22-5)	Imphal West 	
+(22-6)	Senapati 	
+(22-7)	Tamenglong 	
+(22-8)	Thoubal 	
+(22-9)	Ukhrul	
+
+23	Meghalaya	
+(23-1)	East Garo Hills 	
+(23-2)	East Khasi Hills 	
+(23-3)	Jaintia Hills 	
+(23-4)	Ribhoi 	
+(23-5)	South Garo Hills 	
+(23-6)	West Garo Hills 	
+(23-7)	West Khasi Hills	
+
+24	Mizoram	
+(24-1)	Aizawl 	
+(24-2)	Champhai 	
+(24-3)	Kolasib 	
+(24-4)	Lawngtlai 	
+(24-5)	Lunglei 	
+(24-6)	Mamit 	
+(24-7)	Saiha 	
+(24-8)	Serchhip	
+
+25	Nagaland	
+(25-1)	Dimapur 	
+(25-2)	Kiphire 	
+(25-3)	Kohima 	
+(25-4)	Longleng 	
+(25-5)	Mokokchung 	
+(25-6)	Mon 	
+(25-7)	Peren 	
+(25-8)	Phek 	
+(25-9)	Tuensang 	
+(25-10)	Wokha 	
+(25-11)	Zunheboto	
+
+26	Odisha	
+(26-1)	Anugul 	
+(26-2)	Balangir 	
+(26-3)	Baleshwar 	
+(26-4)	Bargarh 	
+(26-5)	Baudh 	
+(26-6)	Bhadrak 	
+(26-7)	Cuttack 	
+(26-8)	Debagarh 	
+(26-9)	Dhenkanal 	
+(26-10)	Gajapati 	
+(26-11)	Ganjam 	
+(26-12)	Jagatsinghapur 	
+(26-13)	Jajapur 	
+(26-14)	Jharsuguda 	
+(26-15)	Kalahandi 	
+(26-16)	Kandhamal 	
+(26-17)	Kendrapara 	
+(26-18)	Kendujhar 	
+(26-19)	Khordha 	
+(26-20)	Koraput 	
+(26-21)	Malkangiri 	
+(26-22)	Mayurbhanj 	
+(26-23)	Nabarangapur 	
+(26-24)	Nayagarh 	
+(26-25)	Nuapada 	
+(26-26)	Puri 	
+(26-27)	Rayagada 	
+(26-28)	Sambalpur 	
+(26-29)	Subarnapur 	
+(26-30)	Sundargarh	
+
+27	Puducherry	
+(27-1)	Karaikal 	
+(27-2)	Mahe 	
+(27-3)	Puducherry 	
+(27-4)	Yanam	
+
+28	Punjab	
+(28-1)	Amritsar 	
+(28-2)	Barnala 	
+(28-3)	Bathinda 	
+(28-4)	Faridkot 	
+(28-5)	Fatehgarh Sahib 	
+(28-6)	Firozpur 	
+(28-7)	Gurdaspur 	
+(28-8)	Hoshiarpur 	
+(28-9)	Jalandhar 	
+(28-10)	Kapurthala 	
+(28-11)	Ludhiana 	
+(28-12)	Mansa 	
+(28-13)	Moga 	
+(28-14)	Muktsar 	
+(28-15)	Patiala 	
+(28-16)	Rupnagar 	
+(28-17)	Sahibzada Ajit Singh Nagar 	
+(28-18)	Sangrur 	
+(28-19)	Shahid Bhagat Singh Nagar 	
+(28-20)	Tarn Taran	
+
+29	Rajasthan	
+(29-1)	Ajmer 	
+(29-2)	Alwar 	
+(29-3)	Banswara 	
+(29-4)	Baran 	
+(29-5)	Barmer 	
+(29-6)	Bharatpur 	
+(29-7)	Bhilwara 	
+(29-8)	Bikaner 	
+(29-9)	Bundi 	
+(29-10)	Chittaurgarh 	
+(29-11)	Churu 	
+(29-12)	Dausa 	
+(29-13)	Dhaulpur 	
+(29-14)	Dungarpur 	
+(29-15)	Jaipur 	
+(29-16)	Jaisalmer 	
+(29-17)	Jalor 	
+(29-18)	Jhalawar 	
+(29-19)	Jhunjhunun 	
+(29-20)	Jodhpur 	
+(29-21)	Karauli 	
+(29-22)	Kota 	
+(29-23)	Nagaur 	
+(29-24)	Pali 	
+(29-25)	Pratapgarh 	
+(29-26)	Rajsamand 	
+(29-27)	Sawai Madhopur 	
+(29-28)	Sikar 	
+(29-29)	Sirohi 	
+(29-30)	Tonk 	
+(29-31)	Udaipur	
+
+30	Sikkim	
+(30-1)	East District 	
+(30-2)	North District 	
+(30-3)	South District 	
+(30-4)	West District	
+
+31	Tamil Nadu	
+(31-1)	Ariyalur 	
+(31-2)	Chennai 	
+(31-3)	Coimbatore 	
+(31-4)	Cuddalore 	
+(31-5)	Dharmapuri 	
+(31-6)	Dindigul 	
+(31-7)	Erode 	
+(31-8)	Kancheepuram 	
+(31-9)	Kanniyakumari 	
+(31-10)	Karur 	
+(31-11)	Krishnagiri 	
+(31-12)	Madurai 	
+(31-13)	Nagapattinam 	
+(31-14)	Namakkal 	
+(31-15)	Perambalur 	
+(31-16)	Pudukkottai 	
+(31-17)	Ramanathapuram 	
+(31-18)	Salem 	
+(31-19)	Sivaganga 	
+(31-20)	Thanjavur 	
+(31-21)	The Nilgiris 	
+(31-22)	Theni 	
+(31-23)	Thiruvallur 	
+(31-24)	Thiruvarur 	
+(31-25)	Thoothukkudi 	
+(31-26)	Tiruchirappalli 	
+(31-27)	Tirunelveli 	
+(31-28)	Tiruppur 	
+(31-29)	Tiruvannamalai 	
+(31-30)	Vellore 	
+(31-31)	Viluppuram 	
+(31-32)	Virudhunagar	
+
+32	Telangana	
+(32-1)	Adilabad 	
+(32-2)	Hyderabad 	
+(32-3)	Karimnagar 	
+(32-4)	Khammam 	
+(32-5)	Mahbubnagar 	
+(32-6)	Medak 	
+(32-7)	Nalgonda 	
+(32-8)	Nizamabad 	
+(32-9)	Rangareddy 	
+(32-10)	Warangal	
+
+33	Tripura	
+(33-1)	Dhalai 	
+(33-2)	North Tripura 	
+(33-3)	South Tripura 	
+(33-4)	West Tripura	
+
+34	Uttar Pradesh	
+(34-1)	Agra 	
+(34-2)	Aligarh 	
+(34-3)	Allahabad 	
+(34-4)	Ambedkar Nagar 	
+(34-5)	Auraiya 	
+(34-6)	Azamgarh 	
+(34-7)	Baghpat 	
+(34-8)	Bahraich 	
+(34-9)	Ballia 	
+(34-10)	Balrampur 	
+(34-11)	Banda 	
+(34-12)	Bara Banki 	
+(34-13)	Bareilly 	
+(34-14)	Basti 	
+(34-15)	Bijnor 	
+(34-16)	Budaun 	
+(34-17)	Bulandshahr 	
+(34-18)	Chandauli 	
+(34-19)	Chitrakoot 	
+(34-20)	Deoria 	
+(34-21)	Etah 	
+(34-22)	Etawah 	
+(34-23)	Faizabad 	
+(34-24)	Farrukhabad 	
+(34-25)	Fatehpur 	
+(34-26)	Firozabad 	
+(34-27)	Gautam Buddha Nagar 	
+(34-28)	Ghaziabad 	
+(34-29)	Ghazipur 	
+(34-30)	Gonda 	
+(34-31)	Gorakhpur 	
+(34-32)	Hamirpur 	
+(34-33)	Hardoi 	
+(34-34)	Jalaun 	
+(34-35)	Jaunpur 	
+(34-36)	Jhansi 	
+(34-37)	Jyotiba Phule Nagar 	
+(34-38)	Kannauj 	
+(34-39)	Kanpur Dehat 	
+(34-40)	Kanpur Nagar 	
+(34-41)	Kanshiram Nagar 	
+(34-42)	Kaushambi 	
+(34-43)	Kheri 	
+(34-44)	Kushinagar 	
+(34-45)	Lalitpur 	
+(34-46)	Lucknow 	
+(34-47)	Mahamaya Nagar 	
+(34-48)	Mahoba 	
+(34-49)	Mahrajganj 	
+(34-50)	Mainpuri 	
+(34-51)	Mathura 	
+(34-52)	Mau 	
+(34-53)	Meerut 	
+(34-54)	Mirzapur 	
+(34-55)	Moradabad 	
+(34-56)	Muzaffarnagar 	
+(34-57)	Pilibhit 	
+(34-58)	Pratapgarh 	
+(34-59)	Rae Bareli 	
+(34-60)	Rampur 	
+(34-61)	Saharanpur 	
+(34-62)	Sant Kabir Nagar 	
+(34-63)	Sant Ravidas Nagar 	
+(34-64)	Shahjahanpur 	
+(34-65)	Shrawasti 	
+(34-66)	Siddharthnagar 	
+(34-67)	Sitapur 	
+(34-68)	Sonbhadra 	
+(34-69)	Sultanpur 	
+(34-70)	Unnao 	
+(34-71)	Varanasi	
+
+35	Uttarakhand	
+(35-1)	Bageshwar 	
+(35-2)	Chamoli 	
+(35-3)	Champawat 	
+(35-4)	Dehradun 	
+(35-5)	Garhwal 	
+(35-6)	Hardwar 	
+(35-7)	Nainital 	
+(35-8)	Pithoragarh 	
+(35-9)	Rudraprayag 	
+(35-10)	Tehri Garhwal 	
+(35-11)	Udham Singh Nagar 	
+(35-12)	Uttarkashi	
+
+36	West Bengal	
+(36-1)	Bankura 	
+(36-2)	Barddhaman 	
+(36-3)	Birbhum 	
+(36-4)	Dakshin Dinajpur 	
+(36-5)	Darjiling 	
+(36-6)	Haora 	
+(36-7)	Hugli 	
+(36-8)	Jalpaiguri 	
+(36-9)	Koch Bihar 	
+(36-10)	Kolkata 	
+(36-11)	Maldah 	
+(36-12)	Murshidabad 	
+(36-13)	Nadia 	
+(36-14)	North Twenty Four Parganas 	
+(36-15)	Paschim Medinipur 	
+(36-16)	Purba Medinipur 	
+(36-17)	Puruliya 	
+(36-18)	South Twenty Four Parganas 	
+(36-19)	Uttar Dinajpur	
+
+")
